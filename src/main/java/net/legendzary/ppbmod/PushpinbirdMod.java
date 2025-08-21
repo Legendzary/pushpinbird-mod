42ca63d39961fc31f51def920f2d5aeeca58d529
deck <legendaryjaydon@gmail.com>
2025-08-20 21:06:48 -0400

    Initial commit

---
+++ src/main/java/net/legendzary/ppbmod/PushpinbirdMod.java
@@ -0,0 +1,16 @@
+package net.legendzary.ppbmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PushpinbirdMod implements ModInitializer {
+	public static final String MOD_ID = "ppbmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/legendzary/ppbmod/PushpinbirdModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.legendzary.ppbmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PushpinbirdModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
